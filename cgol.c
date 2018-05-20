192bab465ca861e3b8b910c82b62374dd7e0fab4
Faruk Acibal <acfaruk@gmail.com>
2018-05-20 16:35:17 +0200

    more input and handle input differently

--- cgol.c
+++ cgol.c
@@ -1,36 +1,55 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
-#include <time.h>
 
-#define WINDOW_SIZE 500
-#define CELLS_IN_AXIS 50
+#define WINDOW_SIZE 1000
+#define CELLS_IN_AXIS 100
+#define MAX_DELAY 500
 
 typedef struct {
 	SDL_Rect rect;
 	char alive;
 } Cell;
 
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Event event;
+
 Cell cells [CELLS_IN_AXIS][CELLS_IN_AXIS];
 Cell oldCells[CELLS_IN_AXIS][CELLS_IN_AXIS];
 
-void initCells()
+int paused = 0;
+int delayTime = 100;
+unsigned int lastTime = 0, currentTime;
+
+int currentMouseGridX = 0;
+int currentMouseGridY = 0;
+
+void addDelayTime(int addedTime) {
+	int newTime = delayTime + addedTime;
+	delayTime = newTime < 0 ? 0 : newTime;
+	delayTime = newTime > MAX_DELAY ? MAX_DELAY : delayTime;
+}
+
+void initCells(int blank)
 {
-	time_t t;
-	srand((unsigned) time(&t));
+	srand(SDL_GetTicks());
 	for (int i = 0; i < CELLS_IN_AXIS; i++)
 	{
 		for (int j = 0; j < CELLS_IN_AXIS; j++)
 		{
-			cells[i][j].alive = rand() % 2;
-			cells[i][j].rect.x = i * 10;
-			cells[i][j].rect.y = j * 10;
-			cells[i][j].rect.h = 10;
-			cells[i][j].rect.w = 10;
+			cells[i][j].alive = blank ? 0 : rand() % 2;
+
+			int cellSize = WINDOW_SIZE / CELLS_IN_AXIS;
+
+			cells[i][j].rect.x = i * cellSize;
+			cells[i][j].rect.y = j * cellSize;
+			cells[i][j].rect.h = cellSize;
+			cells[i][j].rect.w = cellSize;
 		}
 	}
 }
 
-void renderCells(SDL_Renderer *renderer)
+void renderCells()
 {
 	for (int i = 0; i < CELLS_IN_AXIS; i++)
 	{
@@ -41,6 +60,12 @@ void renderCells(SDL_Renderer *renderer)
 			SDL_RenderFillRect(renderer, &cells[i][j].rect);
 		}
 	}
+}
+
+void renderFeintCell(int x, int y) {
+	int cellSize = WINDOW_SIZE / CELLS_IN_AXIS;
+	SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+	SDL_RenderFillRect(renderer, &cells[x][y].rect);
 }
 
 int countNeighbours(int x, int y)
@@ -90,12 +115,51 @@ void updateCells()
 	}
 }
 
+void handleKeyInput(SDL_KeyboardEvent* keyEvent) {
+	switch (keyEvent->keysym.sym) {
+		case SDLK_r:
+			initCells(0);
+			break;
+		case SDLK_c:
+			initCells(1);
+			break;
+		case SDLK_t:
+			paused = !paused;
+			break;
+		case SDLK_a:
+			addDelayTime(-50);
+			break;
+		case SDLK_s:
+			addDelayTime(50);
+			break;
+	}
+}
+
+void handleMouseMotion(SDL_MouseMotionEvent* mouseEvent) {
+	int cellSize = WINDOW_SIZE / CELLS_IN_AXIS;
+	currentMouseGridX =  mouseEvent->x / cellSize;
+	currentMouseGridY = mouseEvent->y / cellSize;
+
+	if (mouseEvent->state & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+		cells[currentMouseGridX][currentMouseGridY].alive = 1;
+	}
+	else if (mouseEvent->state & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
+		cells[currentMouseGridX][currentMouseGridY].alive = 0;
+	}
+}
+
+void handleMouseButton(SDL_MouseButtonEvent* mouseEvent) {
+	if (mouseEvent->button == SDL_BUTTON_LEFT) {
+		cells[currentMouseGridX][currentMouseGridY].alive = 1;
+	}
+	else if (mouseEvent->button == SDL_BUTTON_RIGHT) {
+		cells[currentMouseGridX][currentMouseGridY].alive = 0;
+	}
+}
+
+
 int main()
 {
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-	SDL_Event event;
-
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
 		return 1;
@@ -106,20 +170,36 @@ int main()
 		return 2;
 	}
 
-	initCells();
+	initCells(0);
 
 	while (1) {
 		SDL_PollEvent(&event);
 		if (event.type == SDL_QUIT) {
 			break;
 		}
+		if (event.type == SDL_KEYDOWN) {
+			handleKeyInput(&event.key);
+		}
+		if (event.type == SDL_MOUSEMOTION) {
+			handleMouseMotion(&event.motion);
+		}
+		if (event.type == SDL_MOUSEBUTTONDOWN) {
+			handleMouseButton(&event.button);
+		}
 
-		updateCells();
-		renderCells(renderer);
-		SDL_RenderPresent(renderer);
-		SDL_Delay(100);
+		currentTime = SDL_GetTicks();
+		if (currentTime >= lastTime + delayTime) {
+			if (!paused) {
+				updateCells();
+			}
+			lastTime = currentTime;
+		}
+
 		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
 		SDL_RenderClear(renderer);
+		renderCells();
+		renderFeintCell(currentMouseGridX, currentMouseGridY);
+		SDL_RenderPresent(renderer);
 	}
 
 	SDL_DestroyRenderer(renderer);
--- readme.md
+++ readme.md
@@ -2,6 +2,18 @@
 
 Convay's Game of Life programmed in C using SDL2 for rendering.
 
+## Controls
+```
+t: toggle simulation (pause/continue)
+c: clear screen
+r: reset/randomize cells
+a: simulation speed up
+s: simulation speed down
+
+Left Mouse: Revive Cell
+Right Mouse: Kill cell
+```
+
 ## Compilation
 First SDL2 is needed on your machine:
 
